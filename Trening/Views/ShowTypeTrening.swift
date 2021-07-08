05d68949f532165a51e89322b035f740da428564
ChiterP <63542970+ChiterP@users.noreply.github.com>
2021-07-08 22:37:27 +0300

    добавил удаление типа тренировки

--- Trening/Views/ShowTypeTrening.swift
+++ Trening/Views/ShowTypeTrening.swift
@@ -10,28 +10,40 @@ import SwiftUI
 struct ShowTypeTrening: View {
     @Binding var showModal: Bool
     
-    let typeTrenings : [TypeTrening] = StorageManager.shared.fetchNameTrening()
+    @State var typeTrenings : [TypeTrening] = StorageManager.shared.fetchNameTrening()
     
     var body: some View {
         
         NavigationView {
-        VStack {
-            HStack {
-                Text("Всего типов упражнений: ")
-                Text("\(typeTrenings.count)")
-                    .frame(width: 120, alignment: .leading)
-                
-            }
-            .padding()
-            
-            List(typeTrenings, id: \.self) { trening in
-                NavigationLink(destination: Details(typeTrening: trening))
-                {
-                TreningRow(typeTrening: trening)
+            VStack {
+                HStack {
+                    Text("Всего типов упражнений: ")
+                    Text("\(typeTrenings.count)")
+                        .frame(width: 120, alignment: .leading)
+                    
                 }
-            }.navigationBarTitle("Тренировки")
+                .padding()
+                
+                List {
+                    ForEach(typeTrenings, id: \.self) { trening in
+                        NavigationLink(destination: Details(typeTrening: trening))
+                        {
+                            TreningRow(typeTrening: trening)
+                        }
+                        
+                    }
+                    .onDelete(perform: { indexSet in
+                        indexSet.forEach {
+                            typeTrenings.remove(at: $0)
+                            StorageManager.shared.deleteNameTrening(at: $0)
+                        }
+                    }
+                    )
+                    
+                }
+                .navigationBarTitle("Тренировки")
+            }
         }
-    }
     }
 }
 
